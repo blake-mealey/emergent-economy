bf76ab8a92f5f60edf81b4fd8ae01b02cff9db41
Blake <blakemealey@gmail.com>
2017-04-24 19:07:01 -0600

    Added maximize mode to population chart. Started on trade ratio table display

--- Assets/Scripts/PopulationLineGraph.cs
+++ Assets/Scripts/PopulationLineGraph.cs
@@ -8,9 +8,17 @@ public class PopulationLineGraph : MonoBehaviour {
 
     public GameObject linePrefab;
     public GameObject labelPrefab;
+    public GameObject ratioColumnPrefab;
+    public GameObject ratioLabelPrefab;
 
+    RectTransform marker;
+    GameObject ratiosPanel;
+
+    List<Vector2>[] graphHistories;
+    List<Vector2>[] currentGraphs;
     UILineRenderer[] lines;
     Text[] labels;
+    Text[,] ratioLabels;
     int index;
     float highestValue = 1;
     float prevHighestValue;
@@ -19,20 +27,52 @@ public class PopulationLineGraph : MonoBehaviour {
     public int windowSize;
     public int maxWindowSize;
 
+    float markerRatio = 0;
+    int markerIndex = 0;
+    bool maximized = false;
+    bool initialized = false;
+
+    Rect worldRect;
+
     // Use this for initialization
     void Start () {
         index = 0;
+        windowSize -= 1; // So positioning logic works correctly
+
+        marker = transform.Find("Marker").GetComponent<RectTransform>();
+        ratiosPanel = transform.Find("TradeRatios").gameObject;
     }
 
     IEnumerator UpdateGraph() {
         yield return new WaitForSeconds(1f);
 
-        lines = new UILineRenderer[SimManager.instance.numberOfGroups];
-        labels = new Text[SimManager.instance.numberOfGroups];
+        int groupCount = SimManager.instance.numberOfGroups;
+        lines = new UILineRenderer[groupCount];
+        labels = new Text[groupCount];
+        ratioLabels = new Text[groupCount,groupCount];
+        graphHistories = new List<Vector2>[groupCount];
+        currentGraphs = new List<Vector2>[groupCount];
+        
+        Transform legend = transform.Find("Legend");
 
-        Transform legend = transform.GetChild(0);
+        for (int i = 0; i < groupCount; i++) {
+            GameObject column = Instantiate(ratioColumnPrefab, ratiosPanel.transform);
 
-        for(int i = 0; i < lines.Length; i++) {
+            for (int j = 0; j < groupCount; j++) {
+                if (i != j) {
+                    GameObject label = Instantiate(ratioLabelPrefab, column.transform);
+                    ratioLabels[i, j] = label.GetComponent<Text>();
+                    string c0 = ColorUtility.ToHtmlStringRGB(SimManager.instance.colors[i]);
+                    string c1 = ColorUtility.ToHtmlStringRGB(SimManager.instance.colors[j]);
+                    ratioLabels[i, j].text = string.Format("<color=#{0}>{1}</color> : <color=#{2}>{3}</color>", c0, 1, c1, 1);
+                }
+            }
+        }
+
+        for (int i = 0; i < lines.Length; i++) {
+            graphHistories[i] = new List<Vector2>();
+            currentGraphs[i] = new List<Vector2>();
+
             Color color = SimManager.instance.colors[i];
 
             GameObject line = Instantiate(linePrefab, transform);
@@ -45,19 +85,34 @@ public class PopulationLineGraph : MonoBehaviour {
             line.GetComponent<Outline>().effectColor = color * 0.7f;
 
             GameObject label = Instantiate(labelPrefab, legend);
-            rt = label.GetComponent<RectTransform>();
-            rt.offsetMin = new Vector2(0, 0);
-            rt.offsetMax = new Vector2(0, 20);
-            rt.localPosition = new Vector2(0, 45 - (i * 20));
             labels[i] = label.GetComponent<Text>();
             labels[i].color = color;
             labels[i].GetComponent<Outline>().effectColor = color * 0.7f;
         }
 
+        UpdateWorldRect();
+        initialized = true;
+
         while (true) {
+            for (int i = 0; i < lines.Length; i++) {
+                UILineRenderer lineRenderer = lines[i];
+
+                float value = (float)SimManager.instance.populations[i];
+
+                if(!maximized)
+                    UpdateLabel(i, value);
+
+                if (value > highestValue) {
+                    prevHighestValue = highestValue;
+                    highestValue = value;
+                    SquishPointsVertically();
+                }
+                AddPoint(i, value);
+            }
+
             index++;
             if (fixedWidth) {
-                if (index >= windowSize) {
+                if (index >= windowSize + 2) {
                     index--;
                     ShiftPointsLeft();
                 }
@@ -65,66 +120,153 @@ public class PopulationLineGraph : MonoBehaviour {
                 fixedWidth = index == maxWindowSize;
                 SquishPointsHorizontally();
             }
+            
+            //if(!maximized)
+                UpdateLines();
 
-            for (int i = 0; i < lines.Length; i++) {
-                UILineRenderer lineRenderer = lines[i];
-
-                float value = (float)SimManager.instance.populations[i];
-                if(!labels[i].text.Equals(value.ToString())) {
-                    labels[i].text = value.ToString();
-                }
-                if (value > highestValue) {
-                    prevHighestValue = highestValue;
-                    highestValue = value;
-                    SquishPointsVertically();
-                }
-                AddPoint(lineRenderer, value / highestValue);
-            }
-
+            if (SimManager.instance.GetPopulation() == 0) break;
             yield return new WaitForSeconds(0.25f);
         }
     }
 
     public void SquishPointsVertically() {
-        foreach (UILineRenderer lineRenderer in lines) {
-            var pointList = new List<Vector2>(lineRenderer.Points);
+        foreach (var pointList in currentGraphs) {
             for (int i = 0; i < pointList.Count; i++) {
                 Vector2 v = pointList[i];
                 v.y = v.y * (prevHighestValue / highestValue);
                 pointList[i] = v;
             }
-            lineRenderer.Points = pointList.ToArray();
         }
     }
 
     public void SquishPointsHorizontally() {
-        foreach (UILineRenderer lineRenderer in lines) {
-            var pointList = new List<Vector2>(lineRenderer.Points);
+        foreach (var pointList in currentGraphs) {
             for (int i = 0; i < pointList.Count; i++) {
                 Vector2 v = pointList[i];
                 v.x = (float)i / (float)index;
                 pointList[i] = v;
             }
-            lineRenderer.Points = pointList.ToArray();
         }
     }
 
     public void ShiftPointsLeft() {
-        foreach (UILineRenderer lineRenderer in lines) {
-            var pointList = new List<Vector2>(lineRenderer.Points);
+        foreach (var pointList in currentGraphs) {
             pointList.RemoveAt(0);
             for (int i = 0; i < pointList.Count; i++) {
                 Vector2 v = pointList[i];
                 v.x -= 1f / (float)windowSize;
                 pointList[i] = v;
             }
-            lineRenderer.Points = pointList.ToArray();
         }
     }
 
-    public void AddPoint(UILineRenderer lineRenderer, float y) {
-        var pointList = new List<Vector2>(lineRenderer.Points);
-        pointList.Add(new Vector2((float)index / (fixedWidth ? windowSize : (float)index), y));
-        lineRenderer.Points = pointList.ToArray();
+    public void AddPoint(int lineIndex, float y) {
+        List<Vector2> pointList = currentGraphs[lineIndex];
+        pointList.Add(new Vector2((float)index / (fixedWidth ? windowSize : (float)index), y / highestValue));
+        graphHistories[lineIndex].Add(new Vector2(0f, y));
+    }
+
+    public void UpdateLabel(int lineIndex, float value) {
+        if (!labels[lineIndex].text.Equals(value.ToString())) {
+            labels[lineIndex].text = value.ToString();
+        }
+    }
+
+    public void UpdateLines() {
+        if (!initialized) return;
+        for (int i = 0; i < lines.Length; i++) {
+            if (!maximized) {
+                lines[i].Points = currentGraphs[i].ToArray();
+            } else {
+                List<Vector2> history = graphHistories[i];
+                Vector2[] outPoints = new Vector2[history.Count];
+
+                float xdiv = (float)history.Count - 1;
+                for (int j = 0; j < history.Count; j++) {
+                    var v = history[j];
+                    outPoints[j] = new Vector2((float)j / xdiv, v.y / highestValue);
+                }
+                lines[i].Points = outPoints;
+            }
+        }
+    }
+
+    public void UpdateWorldRect() {
+        Vector3[] corners = new Vector3[4];
+        GetComponent<RectTransform>().GetWorldCorners(corners);
+        worldRect = new Rect(corners[0] + new Vector3(10, 10), corners[2] - corners[0] - new Vector3(20, 20));
+    }
+
+    public void maximize() {
+        // TODO: Pause simulation
+
+        RectTransform rt = GetComponent<RectTransform>();
+        rt.anchorMax = new Vector2(1, 1);
+        rt.offsetMax = new Vector2(-65, rt.offsetMax.y);
+
+        marker.sizeDelta = new Vector2(1, marker.sizeDelta.y);
+
+        ratiosPanel.SetActive(true);
+
+        UpdateWorldRect();
+        UpdateLines();
+    }
+
+    public void minimize() {
+        // TODO: Unpause simulation
+
+        RectTransform rt = GetComponent<RectTransform>();
+        rt.anchorMax = new Vector2(0, 1);
+        rt.sizeDelta = new Vector2(250, 100);
+
+        marker.sizeDelta = new Vector2(0, marker.sizeDelta.y);
+
+        for (int i = 0; i < lines.Length; i++) {
+            var points = currentGraphs[i];
+            UpdateLabel(i, points[points.Count - 1].y);
+        }
+
+        ratiosPanel.SetActive(false);
+
+        UpdateWorldRect();
+        UpdateLines();
+    }
+
+    public void Update() {
+        // Toggle maximized when Tab is pressed
+        if (Input.GetKeyUp(KeyCode.Tab)) {
+            maximized = !maximized;
+            if (maximized) {
+                maximize();
+            } else {
+                minimize();
+            }
+        }
+
+        // Update marker
+        if (maximized) {
+            if (worldRect.Contains(Input.mousePosition)) {
+                float x = Input.mousePosition.x - worldRect.x;
+                float r = x / worldRect.width;
+                marker.position = new Vector2(x + 20, marker.position.y);
+
+                int lineIndex = (int)(r * (float)graphHistories[0].Count);
+                if (lineIndex != markerIndex) {
+                    markerIndex = lineIndex;
+                    markerRatio = r;
+                    for (int i = 0; i < lines.Length; i++) {
+                        UpdateLabel(i, graphHistories[i][lineIndex].y);
+                    }
+                }
+            } else {
+                int lineIndex = (int)(markerRatio * (float)graphHistories[0].Count);
+                if (lineIndex != markerIndex) {
+                    markerIndex = lineIndex;
+                    for (int i = 0; i < lines.Length; i++) {
+                        UpdateLabel(i, graphHistories[i][lineIndex].y);
+                    }
+                }
+            }
+        }
     }
 }
